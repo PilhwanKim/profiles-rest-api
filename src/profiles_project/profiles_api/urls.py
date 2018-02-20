78ec8b57eff33ef7dc0a0a2665583e946e5cdc0f
Leon Kim <pilhwankim03@gmail.com>
2018-02-20 11:31:25 +0900

    Added login API

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -6,8 +6,9 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-# router.register('hello-viewset', views.HelloApiView, base_name='hello-viewset')
+# router.register('hello-view', views.HelloApiView, base_name='hello-view')
 router.register('profile', views.UserProfileViewSet)
+router.register('login', views.LoginViewSet, base_name='login')
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
